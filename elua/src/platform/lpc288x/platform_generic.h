d866bd04ea65538606170ab0bd00722ae0d65b33
MeadowView <mv3142@gmail.com>
2015-08-30 00:04:47 -0700

    updated

--- elua/src/platform/lpc288x/platform_generic.h
+++
@@ -1,9 +0,0 @@
-// Platform customization header
-
-#ifndef __PLATFORM_GENERIC_H__
-#define __PLATFORM_GENERIC_H__
-
-#define PLATFORM_TMR_COUNTS_DOWN
-
-#endif // #ifndef __PLATFORM_GENERIC_H__
-
--- elua/src/platform/str7/platform_generic.h
+++
@@ -1,7 +0,0 @@
-// Platform customization header
-
-#ifndef __PLATFORM_GENERIC_H__
-#define __PLATFORM_GENERIC_H__
-
-#endif // #ifndef __PLATFORM_GENERIC_H__
-
